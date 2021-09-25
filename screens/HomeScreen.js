ce18436a9e7585bb21ef8b740c8860544c112791
sourabh singh <mshrudra@gmail.com>
2021-09-25 21:57:32 +0530

    initial start

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View, Text, SafeAreaView } from "react-native";
+import tw from "tailwind-react-native-classnames";
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>home component ..</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
